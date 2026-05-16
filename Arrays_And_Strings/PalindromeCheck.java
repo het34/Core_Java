937a5b1e486c0d08ddefa6d446fa1b138ee37107
het34 <hettamboli341@gmail.com>
2026-05-17 00:25:55 +0530

    Completed Arrays and String Praticals

---
+++ Arrays_And_Strings/PalindromeCheck.java
@@ -0,0 +1,22 @@
+package Arrays_And_Strings;
+
+class PalindromeCheck {
+
+    public static void main(String[] args) {
+
+        String original = "madam";
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+
+        System.out.println("Reversed String: " + reversed);
+
+        if (original.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
---
+++ Arrays_And_Strings/StringComparison.java
@@ -0,0 +1,5 @@
+package Arrays_And_Strings;
+
+public class StringComparison {
+
+}

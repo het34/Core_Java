56bdd1b76bb8341e14ba82b591249b74477c6f6d
het34 <hettamboli341@gmail.com>
2026-05-17 23:04:33 +0530

    Completed the excercise ofr comparing string

--- Arrays_And_Strings/StringComparison.java
+++ Arrays_And_Strings/StringComparison.java
@@ -1,5 +1,19 @@
 package Arrays_And_Strings;
 
-public class StringComparison {
+class StringComparison {
 
+    public static void main(String[] args) {
+
+        String str1 = "Java";
+        String str2 = "Java";
+        String str3 = "Python";
+
+        System.out.println("Using equals():");
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.equals(str3));
+
+        System.out.println("Using compareTo():");
+        System.out.println(str1.compareTo(str2));
+        System.out.println(str1.compareTo(str3));
+    }
 }

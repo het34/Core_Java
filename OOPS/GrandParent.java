5f27b178264695b60484a291ba61b0bdef7498cb
het34 <hettamboli341@gmail.com>
2026-05-21 00:00:49 +0530

    Added file in OOPs

--- OOPS/GrandParent.java
+++ OOPS/GrandParent.java
@@ -1,5 +1,31 @@
 package OOPS;
 
-public class GrandParent {
+class GrandParent {
 
+    void showGrandParent() {
+        System.out.println("GrandParent Class");
+    }
+}
+
+class Parent2 extends GrandParent {
+
+    void showParent() {
+        System.out.println("Parent Class");
+    }
+}
+
+class Child2 extends Parent2 {
+
+    void showChild() {
+        System.out.println("Child Class");
+    }
+
+    public static void main(String[] args) {
+
+        Child2 c = new Child2();
+
+        c.showGrandParent();
+        c.showParent();
+        c.showChild();
+    }
 }
